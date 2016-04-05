108de54b8170de6df57b9f05c37629b7b75668ef
Oussema Harbi <oussema.elharbi@gmail.com>
2016-04-05 08:08:43 +0100

    Version working on stm32f4 using SDIO
    Signed-off-by: Oussema Harbi <oussema.elharbi@gmail.com>

---
+++ drivers/fatfs_sd.h
@@ -0,0 +1,18 @@
+/*-----------------------------------------------------------------------/
+/  Low level disk interface modlue include file   (C)ChaN, 2013          /
+/-----------------------------------------------------------------------*/
+
+#ifndef _DISKIO_DEFINED_SD
+#define _DISKIO_DEFINED_SD
+
+#define _USE_WRITE	1	/* 1: Enable disk_write function */
+#define _USE_IOCTL	1	/* 1: Enable disk_ioctl fucntion */
+
+#include "diskio.h"
+#include "integer.h"
+
+#include "stm32f4xx_hal.h"
+#include "defines.h"
+
+#endif
+
